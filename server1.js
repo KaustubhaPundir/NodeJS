f9714cc97d5e8cab20e90e93f9a3bf00db7d22ba
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-11 20:15:55 +0530

    added a listener for our server1.js and added comments in it

--- server1.js
+++ server1.js
@@ -1,7 +1,9 @@
-const http=require('http');
+const http=require('http'); //importing http for comms and data transfer
 
 function requestListener(req,res){
     console.log(req);
-}
-const server=http.createServer(requestListener);
-server.listen(3001);
+}//created a function request listner whihc takes a request req and gives a response res
+const server=http.createServer(requestListener); //creates a server which runs our function
+const port=3001; //port for our server
+server.listen(3001,()=>{console.log("Server running");}); // tells server to listen at port 3001 , if success log the message
+//the second arg in server.listen is an anonimous(direct funtion as argument) function which will be called by server.listen
