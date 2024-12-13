4290f5859300c39a71a2313f736f027689b3f6f5
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-13 15:27:38 +0530

    added redirection

--- server3.js
+++ server3.js
@@ -1,20 +1,24 @@
 //taking input from user 
 
 const http = require('http'); //importing http for comms and data transfer
-
+const fs=require('fs');
 function requestListener(req, res) {
     console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<head><title>Server1</title><head>');
     if (req.url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<head><title>Server1</title><head>');
         res.write('<html>');
         res.write('<body><h1>Welcome to Home Page</h1></body>');
         res.write('<form action="/submit-details" method="POST">');//in action we give the url where we have to submit the form and method post means we are coming with some data
         res.write('<input type="text" name="username" id="name" placeholder="Enter your name"><br>');
         res.write('<button type="submit">Submit</button>')
         res.write('</html>');
+    }else if(req.url.toLowerCase()==='/submit-details' && req.method=="POST"){
+        fs.writeFileSync('user-details.txt','kaustubha');
+        res.statusCode=302;//302 means redirection
+        res.setHeader('Location','/'); //change location to /
         return res.end();
-    }
+    } 
 
 }
 const server = http.createServer(requestListener);
