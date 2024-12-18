0ab2df8fe75e72d3cf8788d422b9a8aad0201066
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-18 18:04:58 +0530

    various path middlewares(or route handling)

--- expressJS/app.js
+++ expressJS/app.js
@@ -3,11 +3,11 @@ import express from 'express';
 import requestListener from './server.js';
 
 const app = express();
-app.use('/',(req,res,next)=>{ //we can add path to use app.use for first
+app.use("/",(req,res,next)=>{ //we can add path to use app.use for first
     console.log("first middleware",req.url,req.method);
-    next();
+    next(); //now this next() will not got to submit-detail middleware as it is not applicable
 });
-app.use((req,res,next)=>{
+app.use("/submit-details",(req,res,next)=>{
     console.log("second middleware",req.url,req.method);
     res.send('<h1>Hello ExpressJS</h1>');
 })
@@ -16,3 +16,7 @@ app.use((req,res,next)=>{
 
 const port = 3001;
 app.listen(3001, () => { console.log("Server running at http://localhost:3001/"); }); 
+
+
+//basically by adding path we are doing route handling
+//that app.use will only be active when the specific path have arrived
