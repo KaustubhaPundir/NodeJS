736644849fd7f646f8718e1245efbed76b3b4c32
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-18 17:50:59 +0530

    added / path in app.use

--- expressJS/app.js
+++ expressJS/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 import requestListener from './server.js';
 
 const app = express();
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
     console.log("first middleware",req.url,req.method);
     next();
 });
@@ -11,8 +11,8 @@ app.use((req,res,next)=>{
     console.log("second middleware",req.url,req.method);
     res.send('<h1>Hello ExpressJS</h1>');
 })
-const server = http.createServer(app);
+
 
 
 const port = 3001;
-server.listen(3001, () => { console.log("Server running at http://localhost:3001/"); }); 
+app.listen(3001, () => { console.log("Server running at http://localhost:3001/"); }); 
