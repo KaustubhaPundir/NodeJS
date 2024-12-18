08dcf13cf532c0a6498e91aec8c913acaba669f6
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-18 12:49:12 +0530

    added express and middlewares

--- expressJS/app.js
+++ expressJS/app.js
@@ -1,5 +1,17 @@
 import http from 'http';
+import express from 'express';
 import requestListener from './server.js';
-const server = http.createServer(requestListener);
+
+const app = express();
+app.use((req,res,next)=>{
+    console.log("first middleware",req.url,req.method);
+    next();
+});
+app.use((req,res,next)=>{
+    console.log("second middleware",req.url,req.method);
+})
+const server = http.createServer(app);
+
+
 const port = 3001;
 server.listen(3001, () => { console.log("Server running at http://localhost:3001/"); }); 
